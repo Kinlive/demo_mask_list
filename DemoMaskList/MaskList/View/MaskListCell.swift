acfe08eaa613e0856b787ceefec0539eefb78b2b
Kinlive <>
2020-05-24 19:03:18 +0800

    feat: layout with MaskListViewController and MaskListCell

---
+++ DemoMaskList/MaskList/View/MaskListCell.swift
@@ -0,0 +1,66 @@
+//
+//  MaskListCell.swift
+//  DemoMaskList
+//
+//  Created by Kinlive on 2020/5/24.
+//  Copyright © 2020 com.kinlive. All rights reserved.
+//
+
+import UIKit
+
+class MaskListCell: UITableViewCell {
+
+  lazy var countyLabel: UILabel = {
+    let label = UILabel(frame: .zero)
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
+
+  lazy var maskLabel: UILabel = {
+    let label = UILabel(frame: .zero)
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
+
+  var viewModel: MaskListCellViewModel = MaskListCellViewModel(maskAdult: 0, county: "") {
+    didSet {
+      self.countyLabel.text = "縣市: \(viewModel.county)"
+      self.maskLabel.text = "成人口罩庫存: \(viewModel.numberOfMaskAtAdult)片"
+    }
+  }
+
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    addSubview(countyLabel)
+    addSubview(maskLabel)
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  override func awakeFromNib() {
+      super.awakeFromNib()
+      // Initialization code
+  }
+
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    setConstraints()
+  }
+
+  private func setConstraints() {
+    // constraints of countyLabel
+    countyLabel.topAnchor.constraint(equalTo: topAnchor, constant: 20).isActive = true
+    countyLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10).isActive = true
+    countyLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10).isActive = true
+    countyLabel.bottomAnchor.constraint(equalTo: maskLabel.topAnchor, constant: 20).isActive = true
+
+    // constraints of maskLabel
+    maskLabel.leadingAnchor.constraint(equalTo: countyLabel.leadingAnchor).isActive = true
+    maskLabel.trailingAnchor.constraint(equalTo: countyLabel.trailingAnchor).isActive = true
+    maskLabel.heightAnchor.constraint(equalTo: countyLabel.heightAnchor).isActive = true
+    maskLabel.bottomAnchor.constraint(greaterThanOrEqualTo: bottomAnchor, constant: -10).isActive = true
+  }
+
+}
--- DemoMaskList/MaskList/View/MaskListViewController.swift
+++ DemoMaskList/MaskList/View/MaskListViewController.swift
@@ -10,11 +10,95 @@ import UIKit
 
 class MaskListViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+  lazy var tableView: UITableView = {
+    let table = UITableView(frame: .zero)
+    table.register(MaskListCell.self, forCellReuseIdentifier: String(describing: MaskListCell.self))
+    table.translatesAutoresizingMaskIntoConstraints = false
+    return table
+  }()
 
+  lazy var indicatorView: UIActivityIndicatorView = {
+    let view = UIActivityIndicatorView(style: .large)
+    view.translatesAutoresizingMaskIntoConstraints = false
+    view.color = .darkGray
+    return view
+  }()
+
+  lazy var viewModel: MaskListViewModel = {
+    let vm = MaskListViewModel(events: .init(onReloadData: {
+      DispatchQueue.main.async {
+        self.tableView.reloadData()
+      }
+    }, onFetchError: { (error) in
+      DispatchQueue.main.async {
+        self.showAlert(error: error)
+      }
+
+    }, isLoading: { (isLoading) in
+      DispatchQueue.main.async {
+        isLoading ? self.indicatorView.startAnimating() : self.indicatorView.stopAnimating()
+      }
+    }))
+
+    return vm
+  }()
+
+  override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+    view.addSubview(tableView)
+    view.addSubview(indicatorView)
+    tableView.delegate = self
+    tableView.dataSource = self
+  }
+
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    viewModel.startFetch()
+  }
+
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    setConstraints()
+  }
+
+  func setConstraints() {
+    tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+    tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+    tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+
+    indicatorView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    indicatorView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+  }
+
+  func showAlert(error: ApiError) {
+    let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+    let ok = UIAlertAction(title: "確認", style: .default, handler: nil)
+    alert.addAction(ok)
+    present(alert, animated: true, completion: nil)
+  }
+
+}
+
+extension MaskListViewController: UITableViewDelegate, UITableViewDataSource {
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return viewModel.output.numberOfItems
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: MaskListCell.self)) as? MaskListCell else { return UITableViewCell() }
+
+    cell.viewModel = viewModel.output.cellViewModel(at: indexPath)
+
+    return cell
+
+  }
+
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 120
+  }
 
 }
 
